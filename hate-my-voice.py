d0a50fba688a937f8f38ba75802e26eeb34476fc
wallacy <wallacevieirada88@gmail.com>
2021-03-20 02:01:15 -0300

    first commit

--- hate-my-voice.py
+++ hate-my-voice.py
@@ -2,12 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select 
 from selenium.webdriver.common.keys import Keys
 
+f = open("teste")
+
 driver = webdriver.Chrome()
 driver.get("https://ttsmp3.com/")
 
 element = driver.find_element_by_id("voicetext")
 element.clear()
-element.send_keys("olá")
+element.send_keys(f.read())
+# element.send_keys("olá")
 element.send_keys(Keys.RETURN)
 
 element = driver.find_element_by_xpath("//*[@id='sprachwahl']")
