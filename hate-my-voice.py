a9a148a1cc5e4785d082dd90c9712e6f01d4353b
wallacy <45300169+wlcsv@users.noreply.github.com>
2021-03-20 02:08:59 -0300

    Update hate-my-voice.py

--- hate-my-voice.py
+++ hate-my-voice.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select 
 from selenium.webdriver.common.keys import Keys
 
-f = open("teste")
+file = open("teste")
 
 driver = webdriver.Chrome()
 driver.get("https://ttsmp3.com/")
@@ -14,10 +14,10 @@ element.send_keys(f.read())
 element.send_keys(Keys.RETURN)
 
 element = driver.find_element_by_xpath("//*[@id='sprachwahl']")
-b = driver.find_element_by_xpath("//*[@id='sprachwahl']/option[5]")
-b.click()
+voiceSelector = driver.find_element_by_xpath("//*[@id='sprachwahl']/option[5]")
+voiceSelector.click()
 
-a = driver.find_element_by_xpath("//*[@id='vorlesenbutton']")
-a.click()
+voice = driver.find_element_by_xpath("//*[@id='vorlesenbutton']")
+voice.click()
 
 # driver.'close()	
